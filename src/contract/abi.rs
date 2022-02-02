15fbe3bfb31ac3287448d465857ab406da211477
3lpsy <8757851+3lpsy@users.noreply.github.com>
2022-02-02 00:54:17 -0800

    day13.1 added 1155 additional functions to abi

--- src/contract/abi.rs
+++ src/contract/abi.rs
@@ -52,7 +52,12 @@ abigen!(
 abigen!(
     ERC1155,
     r#"[
+        function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external
+        function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external
         function balanceOf(address _owner) external view returns (uint256)
+        function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory)
+        function setApprovalForAll(address _operator, bool _approved) external
+        function isApprovedForAll(address _owner, address _operator) external view returns (bool)
     ]"#
 );
 
