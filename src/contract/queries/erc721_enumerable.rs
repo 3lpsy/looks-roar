ee0f6daf9444718d51a25f1a359a68d2705e74b7
3lpsy <8757851+3lpsy@users.noreply.github.com>
2022-02-08 00:36:29 -0800

    day16.1 - async batched

--- src/contract/queries/erc721_enumerable.rs
+++ src/contract/queries/erc721_enumerable.rs
@@ -15,19 +15,25 @@ impl ERC721EnumerableQuery {
     ) -> Result<Vec<U256>, Box<dyn Error>> {
         match imp.total_supply().call().await {
             Ok(total_supply) => {
+                let mut tokens: Vec<U256> = vec![];
                 let total_supply_willing: u64 = total_supply.as_u64();
-                let range = 0..total_supply_willing;
-                let tokens = join_all(range.into_iter().map(|index| {
-                    let imp = &imp;
-                    let uindex = U256::from(index);
-                    async move {
-                        //...
-                        let token = imp.token_by_index(uindex).call().await.unwrap();
-                        println!("Token: {:?}", token.clone());
-                        token
-                    }
-                }))
-                .await;
+                let mut range = (0..total_supply_willing).peekable();
+                while range.peek().is_some() {
+                    let batch = range.by_ref().take(100);
+                    let futs = batch.into_iter().map(|index| {
+                        let imp = &imp;
+                        let uindex = U256::from(index);
+                        async move {
+                            //...
+                            let token = imp.token_by_index(uindex).call().await.unwrap();
+                            println!("Token: {:?}", token.clone());
+                            token
+                        }
+                    });
+                    let mut found = join_all(futs).await;
+                    tokens.append(&mut found);
+                }
+                println!("len: {:?}", tokens.len());
                 Ok(tokens)
             }
             Err(e) => Err(AppError::boxed(
