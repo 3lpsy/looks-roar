4497d81235e39cbb16aeda9781f3567bace1870a
3lpsy <8757851+3lpsy@users.noreply.github.com>
2022-02-18 00:47:00 -0800

    day 23 - use call raw to get responses

--- src/contract/abi.rs
+++ src/contract/abi.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use super::constants;
@@ -5,8 +6,8 @@ use super::constants;
 use crate::contract::abis::ERC165;
 use crate::contract::types::NFTIface;
 use crate::utils::AppError;
+use ethers::abi::Tokenizable;
 use ethers::core::types::Address;
-use ethers::prelude::builders::ContractCall;
 use ethers::prelude::Multicall;
 // use ethers::prelude::Signer;
 // use ethers::prelude::U256;
@@ -44,13 +45,14 @@ impl<M: Middleware> NFTAbi<M> {
     pub async fn guess_nft_ifaces(
         addresses: Vec<Address>,
         provider: Arc<M>,
-    ) -> Result<Vec<NftIfacesResponse>, Box<dyn Error>> {
-        let responses =
-            Self::guess_ifaces_or_unsupported(addresses, NFTIface::all_ids(), provider).await?;
+    ) -> Result<HashMap<Address, Vec<[u8; 4]>>, Box<dyn Error>> {
+        let responses = Self::guess_ifaces(addresses, NFTIface::all_ids(), provider).await?;
         let mut missing: Vec<Address> = vec![];
-        for response in responses {
-            if !response.ifaces.is_empty() {
-                missing.push(response.address)
+
+        // loop over map
+        for (addr, ifaces) in &responses {
+            if ifaces.is_empty() {
+                missing.push(*addr)
             }
         }
         if !missing.is_empty() {
@@ -59,19 +61,19 @@ impl<M: Middleware> NFTAbi<M> {
                 0,
             ));
         }
-        unimplemented!()
+        Ok(responses)
     }
 
     // ifaces is probably going to be less than 1000 calls in a multicall
-    pub async fn guess_ifaces_or_unsupported(
+    pub async fn guess_ifaces(
         addresses: Vec<Address>,
         ifaces: Vec<[u8; 4]>,
         provider: Arc<M>,
-    ) -> Result<Vec<IfaceResponse>, Box<dyn Error>> {
+    ) -> Result<HashMap<Address, Vec<[u8; 4]>>, Box<dyn Error>> {
         // M is included in potential error so can't use '?'
         // multicall is restricted to 16 at a time
-        let mut m = Multicall::new(provider.clone(), None).await.unwrap();
-        let mut calls: Vec<ContractCall<M>> = vec![];
+        let m = Multicall::new(provider.clone(), None).await.unwrap();
+        let mut queries: Vec<(Address, [u8; 4])> = vec![];
         let multi = addresses
             .iter()
             .map(|a| {
@@ -80,18 +82,38 @@ impl<M: Middleware> NFTAbi<M> {
             })
             .fold(m, |mut multi_carry, contract| {
                 for iface in &ifaces[..] {
+                    queries.push((contract.address().clone(), iface.clone()));
                     multi_carry.add_call(contract.supports_interface(*iface));
                 }
                 multi_carry
             });
 
-        //
-        let data: Vec<bool> = match multi.call().await {
-            Ok(result) => result,
+        // TODO: does it err if 165 is unsupported?
+        let responses: Vec<bool> = match multi.call_raw().await {
+            Ok(result) => result
+                .iter()
+                .map(|t| bool::from_token(t.to_owned()).unwrap())
+                .collect(),
             Err(_e) => return Err(AppError::boxed("Multicall failed".to_string(), 0)),
         };
-        dbg!(data);
-        Ok(vec![])
+
+        assert!(responses.len() == queries.len());
+
+        let res = responses
+            .iter()
+            .zip(queries.iter())
+            .map(|(status, query)| (query.0, query.1, status))
+            .fold(HashMap::new(), |mut carry, (addr, iface, status)| {
+                carry.entry(addr).or_insert_with(Vec::new);
+                let entry = carry.get_mut(&addr).unwrap();
+                if *status {
+                    // only append true ifaces
+                    entry.push(iface);
+                }
+                carry
+            });
+
+        Ok(res)
     }
 
     // just handle both cases
