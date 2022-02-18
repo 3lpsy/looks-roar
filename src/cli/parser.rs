ef8335413ac903127b3958f13fecabe983eafae9
3lpsy <8757851+3lpsy@users.noreply.github.com>
2022-02-18 02:50:56 -0800

    change cache name to db and update readme

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -10,13 +10,13 @@ pub fn parse() -> clap::ArgMatches {
     let arg_contract = Arg::new("contract")
         .short('c')
         .long("contract")
-        .help("contract to target")
+        .help("contract to target (can be '0xABC' string or path to file)")
         .required(true)
         .takes_value(true);
     let arg_provider = Arg::new("provider")
         .short('p')
         .long("provider")
-        .help("ethereum provider")
+        .help("ethereum provider (such as 'http://localhost:8545'")
         .takes_value(true);
     let arg_db = Arg::new("db")
         .short('d')
